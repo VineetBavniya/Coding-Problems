2677534d5217609cfe582cc5b446028f466b09d0
VineetBavniya <138431446+VineetBavniya@users.noreply.github.com>
2024-09-10 14:17:27 +0530

    Create Print1ToNWithoutLoop.cpp

---
+++ Recursion/Print1ToNWithoutLoop.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define endl "\n"
+#define ll long long 
+#define llu unsigned long long 
+#define lli long long int 
+const int NUM = 10001;
+const ll MOD = 1e9+7;
+
+
+
+void solve(int n){
+	
+	if(n < 1) return;
+
+	solve(n -1);
+	cout << n << " ";
+}
+
+
+int main()
+{
+
+   	#ifndef ONLINE_JUDGE
+   		freopen("input.txt", "r", stdin);
+   	 	freopen("output.txt", "w", stdout);
+	#endif
+
+   	int n; cin>>n;
+	solve(n); 	
+	
+   	return 0;
+	
+}
