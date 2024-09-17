fa20f05356d040e5b80a877c61a11b70b117d780
VineetBavniya <138431446+VineetBavniya@users.noreply.github.com>
2024-09-17 13:40:20 +0530

    Create CeJeopardy.cpp

---
+++ OnlineJudge/SuperEasy/CeJeopardy.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+typedef long long int lli; 
+typedef unsigned long long llu;
+typedef vector<int> vi;
+typedef pair<int, int> pi; 
+
+#define ff first 
+#define ss second 
+#define pb push_back
+#define pop pop_back
+#define mkp make_pair
+
+const int NUM = 10001;
+const ll MOD = 1e9+7;
+
+
+
+void solve(){
+	char ch;
+	while((ch = getchar()) != EOF){
+		printf("%c", ch);
+	}
+}
+
+
+
+int main()
+{
+
+   	#ifndef ONLINE_JUDGE
+   		freopen("input.txt", "r", stdin);
+   	 	freopen("output.txt", "w", stdout);
+	#endif
+   	solve();
+   	return 0;
+	
+}
